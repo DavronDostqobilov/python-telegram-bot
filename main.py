8877c416b10cf8ee115108ed7702ed8d39d4eec1
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-02-21 13:27:43 +0500

    class_echo

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import telegram
+import time
 import os
+import pprint
 
 TOKEN=os.environ['TOKEN']
 bot = telegram.Bot(TOKEN)
 
-update = bot.getUpdates()[-1]
-update_id = update.update_id
-text = update.message.text
 
-chat_id = update.message.chat.id
+last_update_id=-1
 
-bot.sendMessage(chat_id, text)
+while True:
+    update = bot.getUpdates()[-1]
+    update_id = update.update_id
+    chat_id = update.message.chat.id
+    last_update=update.message
+    print(last_update)
+    
+    if update_id != last_update_id:
+        if  update.message.sticker:
+            sticker=update.message.sticker.file_id
+            bot.sendSticker(chat_id,sticker)
+        elif update.message.photo:
+            photo=update.message.photo[0]['file_id']
+            bot.sendPhoto(chat_id, photo)
+        elif update.message.text:
+            text = update.message.text
+            bot.sendMessage(chat_id, text)
+        
+        
+        last_update_id = update_id
+    time.sleep(2)
